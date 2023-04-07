2d5e46292912f2229bd4050f738f89034dfeadc7
Manjul <manjultamrakar4@gmail.com>
2023-04-07 13:59:02 +0545

    added methods

--- script.js
+++ script.js
@@ -10,15 +10,15 @@
 
 class DOMInterface {
     constructor() {
-        this.form = document.querySelector("#comic-form")
-        this.searchField = document.querySelector("#search-input")
+        this.form = document.querySelector("#comic-form");
+        this.searchField = document.querySelector("#search-input");
 
-        this.title = document.querySelector("#comic-title")
-        this.image = document.querySelector("#comic-image")
+        this.title = document.querySelector("#comic-title");
+        this.image = document.querySelector("#comic-image");
 
-        this.error = document.querySelector("#error")
-        this.formError = document.querySelector("#form-error")
-        this.loader = document.querySelector("#loader")
+        this.error = document.querySelector("#error");
+        this.formError = document.querySelector("#form-error");
+        this.loader = document.querySelector("#loader");
 
         this.controls = {
             previous: document.querySelector("#request-prev"),
@@ -27,5 +27,48 @@ class DOMInterface {
             first: document.querySelector("#request-first"),
             last: document.querySelector("#request-last")
         };
+    };
+    
+    clearResults() {
+        this.title.innerHTML = "Loading ...";
+        this.image.src = "";
+        this.image.alt = "";
+    };
+
+    hideLoader() {
+        this.loader.classList.remove('d-flex');
+        this.loader.classList.add("d-none");
+
+    };
+
+    showLoader() {
+        this.loader.classList.remove('d-none');
+        this.loader.classList.add("d-flex");
+    };
+
+    showError() {
+        this.hideLoader();
+        this.formError.innerHTML = "There has been an error, try again!";
+    };
+
+    showFormError(message) {
+        this.hideLoader();
+        this.formError.innerHTML = message;
+    }
+
+    hideErrors() {
+        this.error.innerHTML = "";
+        this.formError.innerHTML = "";
+    };
+
+    showComics(data) {
+        const { title, img } = data;
+        
+        this.title.innerHTML = title;
+        this.image.src = img;
+
+        if(data.alt) this.image.alt = data.alt;
+
+        this.hideLoader();
     }
 }
